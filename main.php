a033eacf882ed40f408ab0e2aa98c338fb3c9a75
kenbonzaiii <esmenakenneth33@gmail.com>
2026-03-04 03:06:27 +0800

    this is stupiddd

--- main.php
+++ main.php
@@ -12,7 +12,7 @@ if (php_sapi_name() === 'cli-server') {
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
 
-require_once __DIR__ . 'Core/autoload.php';
+require_once __DIR__ . '/Core/autoload.php';
 
 use Core\Http\Request;
 use Core\Http\Router;
